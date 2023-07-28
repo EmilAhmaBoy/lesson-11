a1a54a6b4bedf608ebd5d849c400806a00e2cdfc
EmilAhmaBoy <emilahmaboy@gmail.com>
2023-07-28 20:58:17 +0300

    Изменено название таблицы базы данных

--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Advertisement(models.Model):
+    class Meta:
+        db_table = 'advertisements'
+
     title = models.CharField('Заголовок', max_length=128)
     description = models.TextField('Описание')
     price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
