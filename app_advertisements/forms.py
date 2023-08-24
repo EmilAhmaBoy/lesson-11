e5992fe5f1a44e5636d684c056573adfc2335ec1
EmilAhmaBoy <emilahmaboy@gmail.com>
2023-08-24 18:57:34 +0300

    Добавлена валидация на поле title

--- app_advertisements/forms.py
+++ app_advertisements/forms.py
@@ -10,13 +10,21 @@ from .models import Advertisement
 #     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control form-control-lg'}))
 
 class AdvertisementForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['description'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['image'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['price'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['auction'].widget.attrs['class'] = 'form-check-input'
+
     class Meta:
         model = Advertisement
-        fields = ['title', 'description', 'price', 'auction', 'image']
+        fields = ('title', 'description', 'price', 'auction', 'image')
 
-    title = forms.CharField(max_length=64, widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
-    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control form-control-lg'}))
-    price = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control form-control-lg'}))
-    auction = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}), required=False)
-    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control form-control-lg'}))
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if title.startswith('?'):
+            raise forms.ValidationError('Заголовок не может начинаться с вопросительного знака!')
+        return title
 
